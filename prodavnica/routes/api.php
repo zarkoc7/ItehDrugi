a8988dd5490a6bf9447508b896a76fcbc1a978f6
zarkoc7 <zarko.rm7@gmail.com>
2023-05-17 22:02:55 +0200

    dodate rute

--- prodavnica/routes/api.php
+++ prodavnica/routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController1;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +23,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users', [UserController1::class,'index']);
+Route::get('/users/{id}', [UserController1::class,'show']);
+
+Route::get('/products', [ProductController::class,'index']);
+Route::get('/products/{id}', [ProductController::class,'show']);
+
+Route::get('/categories', [CategoryController::class,'index']);
+Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
+
+
+
