3e3c17cd7b2ec8a2ec08b6fb8e095892759ff090
zarkoc7 <zarko.rm7@gmail.com>
2023-05-17 16:48:51 +0200

    dodata migracija za unique i seederi

--- prodavnica/database/seeders/DatabaseSeeder.php
+++ prodavnica/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Manufacturer;
+use App\Models\Product;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,100 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::Truncate();
+        Product::Truncate();
+        Category::Truncate();
+        Manufacturer::Truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+         $user =User::factory(4)->create();
+
+         $u5 = User::create([
+            'name'=>'Zarko',
+            'email'=>'zarkoc@gmail.com',
+            'password'=>bcrypt('sifra')
+        ]);
+
+        
+        $u6 = User::create([
+            'name'=>'Marko',
+            'email'=>'markom@gmail.com',
+            'password'=>bcrypt('sifra')
+        ]);
+
+        $u7 = User::create([
+            'name'=>'Darko',
+            'email'=>'darkom@gmail.com',
+            'password'=>bcrypt('sifra')
+        ]);
+
+        $c1 = Category::create([
+            'name'=>'High-end',
+            'description'=>'roba visokog kvaliteta'
+        ]);
+        $c2 = Category::create([
+            'name'=>'Prosecna',
+            'description'=>'roba prosecnog kvaliteta'
+        ]);
+
+        $c3 = Category::create([
+            'name'=>'low-end',
+            'description'=>'roba niskog kvaliteta'
+        ]);
+
+        $b1 = Manufacturer::create([
+            'name'=>'Rolex',
+            'country'=> 'Svajcarska',
+            'category_id'=>$c1->id
+        ]);
+
+        $b2 = Manufacturer::create([
+            'name'=>'Casio',
+            'country'=> 'Japan',
+            'category_id'=>$c2->id
+        ]);
+
+        $b3 = Manufacturer::create([
+            'name'=>'Daniel Klein',
+            'country'=> 'Ceska',
+            'category_id'=>$c3->id
+        ]);
+
+        $p1 = Product::create([
+            'product_name'=>'Rolex submariner',
+            'description'=> 'automatski, mehanicki, samonavijajuci, oyster 41mm',
+            'price'=>10200,
+            'manufacturer_id'=>$b1->id,
+            'user_id'=>$u5->id
+        ]);
+
+        $p1 = Product::create([
+            'product_name'=>'Casio edifice',
+            'description'=> 'Celik, rucni sat, 51mm',
+            'price'=>200,
+            'manufacturer_id'=>$b2->id,
+            'user_id'=>$u6->id
+        ]);
+
+        $p1 = Product::create([
+            'product_name'=>'Daniel clein classic',
+            'description'=> 'metal, 45mm, mineralni kristal',
+            'price'=>50,
+            'manufacturer_id'=>$b3->id,
+            'user_id'=>$u7->id
+        ]);
+
+
+
+
+
+
+
+
+
+
+
+
+      
     }
 }
